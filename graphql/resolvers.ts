3224166ed7512985a1d2e59ca55271f67ef8cc26
Edier Aristizábal <edieraristizabal@gmail.com>
2021-12-17 20:53:47 -0500

    delete

--- graphql/resolvers.ts
+++
@@ -1,8 +0,0 @@
-import { resolversProyecto } from "../models/proyecto/resolvers";
-import { resolversUsuario } from "../models/usuario/resolvers";
-import { resolversAvance } from '../models/avance/resolvers.js';
-import { resolverInscripciones } from '../models/inscripcion/resolvers.js';
-
-export const resolvers = [ resolversUsuario, resolversProyecto, resolversAvance, resolverInscripciones ];
-
-
